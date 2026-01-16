ef64aa1463b96a2f87000c8896940c4a373d3de4
Bryce Farley <brycesfarley@gmail.com>
2026-01-16 12:23:39 -0700

    impl(p0) create moves calculator classes

---
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -0,0 +1,12 @@
+package chess;
+
+import java.util.Collection;
+import java.util.List;
+
+public class PieceMovesCalculator {
+    // get
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        return List.of();
+    }
+
+}
