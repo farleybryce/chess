abed3f7f5770513573d98a6e11a830bcea3090b7
Bryce Farley <brycesfarley@gmail.com>
2026-01-19 21:51:19 -0700

    impl(p0) start pawn movement

--- shared/src/main/java/chess/PawnMovesCalculator.java
+++ shared/src/main/java/chess/PawnMovesCalculator.java
@@ -1,4 +1,18 @@
 package chess;
 
-public class PawnMovesCalculator extends PieceMovesCalculator {
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class PawnMovesCalculator {
+    public static Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition, ChessPiece piece) {
+        // list of all the legal moves the Pawn can make
+        ArrayList<ChessMove> legalMoves = new ArrayList<>();
+        // get row and column coordinates of Pawn
+        int myRow = myPosition.getRow();
+        int myCol = myPosition.getColumn();
+
+
+
+        return legalMoves;
+    }
 }
