a460a3a2d88c7edb05b5f548d277ba7b8023f994
Bryce Farley <brycesfarley@gmail.com>
2026-01-19 18:01:21 -0700

    impl(p0) added checkLineOfSight function to SquareValid

--- shared/src/main/java/chess/SquareValid.java
+++ shared/src/main/java/chess/SquareValid.java
@@ -3,7 +3,7 @@ package chess;
 import java.util.ArrayList;
 
 public class SquareValid {
-    public static boolean squareValid (int y, int x, ArrayList<ChessMove> legalMoves, ChessBoard board, ChessPiece piece, ChessPosition myPosition) {
+    public static boolean squareValid(int y, int x, ArrayList<ChessMove> legalMoves, ChessBoard board, ChessPiece piece, ChessPosition myPosition) {
         ChessPosition targetPosition = new ChessPosition(y, x);
         ChessPiece targetPiece = board.getPiece(targetPosition);
         // check if target space is occupied by piece of the same color
@@ -11,6 +11,26 @@ public class SquareValid {
             legalMoves.add(new ChessMove(myPosition, targetPosition, null));
             return true;
         } else { return false; }
+    }
+
+    public static boolean checkLineOfSight(int row, int col, int rowVar, int colVar, ArrayList<ChessMove> legalMoves, ChessBoard board, ChessPiece piece, ChessPosition myPosition) {
+        ChessPosition targetPosition = new ChessPosition(row, col);
+        ChessPiece targetPiece = board.getPiece(targetPosition);
+        //check if looking at current position
+        if (targetPosition.equals(myPosition)) {
+            return checkLineOfSight(row + rowVar, col + colVar, rowVar, colVar, legalMoves, board, piece, myPosition);
+        // check if looking out of bounds
+        } else if ((row < 1) || (8 < row) || (col < 1) || (8 < col)) {
+            return true;
+        // check if looking at empty square
+        } else if (targetPiece == null) {
+            legalMoves.add(new ChessMove(myPosition, targetPosition, null));
+            return checkLineOfSight(row + rowVar, col + colVar, rowVar, colVar, legalMoves, board, piece, myPosition);
+        // check if looking at square with capturable piece
+        } else if (targetPiece.getTeamColor() != piece.getTeamColor()) {
+            legalMoves.add(new ChessMove(myPosition, targetPosition, null));
+            return true;
+        } else { return true; }
     }
 
 }
