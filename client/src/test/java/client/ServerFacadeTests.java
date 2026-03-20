5ed76a097cc0448f4984a634f81236dd88b38ec1
Bryce Farley <brycesfarley@gmail.com>
2026-03-20 15:20:53 -0600

    impl(p5) set up server facade testing and wrote first (register) test

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,18 +1,28 @@
 package client;
 
+import dataaccess.DataAccessException;
 import org.junit.jupiter.api.*;
+import server.RegisterRequest;
 import server.Server;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade(String.format("http://localhost:%d", port));
+    }
+
+    @BeforeEach
+    public void clearDB() throws DataAccessException {
+        facade.clear();
     }
 
     @AfterAll
@@ -20,10 +30,10 @@ public class ServerFacadeTests {
         server.stop();
     }
 
-
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    public void successfulRegisterTest() throws DataAccessException {
+        var registerResult = facade.register(new RegisterRequest("player1", "password", "p1@email.com"));
+        assertTrue(registerResult.authToken().length() > 10);
     }
 
 }
