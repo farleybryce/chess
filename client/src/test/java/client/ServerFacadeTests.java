fe29d3a16863d7e73803a6ba46d520aa0a18582f
Bryce Farley <brycesfarley@gmail.com>
2026-03-20 15:43:32 -0600

    impl(p5) tests for handling users created and all pass

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -2,6 +2,8 @@ package client;
 
 import dataaccess.DataAccessException;
 import org.junit.jupiter.api.*;
+import server.LoginRequest;
+import server.RegisterLoginResult;
 import server.RegisterRequest;
 import server.Server;
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,6 +13,7 @@ public class ServerFacadeTests {
 
     private static Server server;
     private static ServerFacade facade;
+    private RegisterLoginResult registerLoginResult;
 
     @BeforeAll
     public static void init() {
@@ -23,6 +26,7 @@ public class ServerFacadeTests {
     @BeforeEach
     public void clearDB() throws DataAccessException {
         facade.clear();
+        registerLoginResult = facade.register(new RegisterRequest("player1", "password", "p1@email.com"));
     }
 
     @AfterAll
@@ -32,8 +36,45 @@ public class ServerFacadeTests {
 
     @Test
     public void successfulRegisterTest() throws DataAccessException {
-        var registerResult = facade.register(new RegisterRequest("player1", "password", "p1@email.com"));
-        assertTrue(registerResult.authToken().length() > 10);
+        assertTrue(registerLoginResult.authToken().length() > 10);
+    }
+
+    @Test
+    public void failedRegisterTest() throws DataAccessException {
+        assertThrows(DataAccessException.class, () -> {
+            facade.register(new RegisterRequest("player1", "password1", "player1@email.com"));
+        });
+    }
+
+    @Test
+    void successfulLoginTest() throws DataAccessException {
+        var loginResult = facade.login(new LoginRequest("player1", "password"));
+        assertTrue(loginResult.authToken().length() > 10);
+    }
+
+    @Test
+    void failedLoginTest() throws DataAccessException {
+        assertThrows(DataAccessException.class, () -> {
+            facade.login(new LoginRequest("player1", "password1"));
+        });
+        assertThrows(DataAccessException.class, () -> {
+            facade.login(new LoginRequest("player2", "password"));
+        });
+    }
+
+    @Test
+    void successfulLogoutTest() throws DataAccessException {
+        facade.logout(registerLoginResult.authToken());
+        assertThrows(DataAccessException.class, () -> {
+            facade.logout(registerLoginResult.authToken());
+        });
+    }
+
+    @Test
+    void failedLogoutTest() throws DataAccessException {
+        assertThrows(DataAccessException.class, () -> {
+            facade.logout("ThisIsNotTheCorrectAuthToken");
+        });
     }
 
 }
