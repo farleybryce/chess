a0aeca52a1e615fd4f80a5778ee08590f13f04e0
Bryce Farley <brycesfarley@gmail.com>
2026-03-20 12:57:38 -0600

    impl(p5) brought over petshop example and added user facade elements

---
+++ client/src/main/java/client/ServerFacade.java
@@ -0,0 +1,114 @@
+package client;
+
+import com.google.gson.Gson;
+import dataaccess.DataAccessException;
+import server.LoginRequest;
+import server.RegisterLoginResult;
+import server.RegisterRequest;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublisher;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+public class ServerFacade {
+    private final HttpClient client = HttpClient.newHttpClient();
+    private final String serverUrl;
+
+    public ServerFacade(String url) {
+        serverUrl = url;
+    }
+
+    public RegisterLoginResult register(RegisterRequest registerRequest) throws DataAccessException {
+        var request = buildRequest("POST", "/user", registerRequest);
+        var response = sendRequest(request);
+        return handleResponse(response, RegisterLoginResult.class);
+    }
+
+    public RegisterLoginResult login(LoginRequest loginRequest) throws DataAccessException {
+        var request = buildRequest("POST", "/session", loginRequest);
+        var response = sendRequest(request);
+        return handleResponse(response, RegisterLoginResult.class);
+    }
+
+    public void logout() throws DataAccessException {
+        var request = buildRequest("DELETE", "/session", null);
+        var response = sendRequest(request);
+        handleResponse(response, null);
+    }
+
+    public Pet addPet(Pet pet) throws DataAccessException {
+        var request = buildRequest("POST", "/pet", pet);
+        var response = sendRequest(request);
+        return handleResponse(response, Pet.class);
+    }
+
+    public void deletePet(int id) throws DataAccessException {
+        var path = String.format("/pet/%s", id);
+        var request = buildRequest("DELETE", path, null);
+        var response = sendRequest(request);
+        handleResponse(response, null);
+    }
+
+    public void deleteAllPets() throws DataAccessException {
+        var request = buildRequest("DELETE", "/pet", null);
+        sendRequest(request);
+    }
+
+    public PetList listPets() throws DataAccessException {
+        var request = buildRequest("GET", "/pet", null);
+        var response = sendRequest(request);
+        return handleResponse(response, PetList.class);
+    }
+
+    private HttpRequest buildRequest(String method, String path, Object body) {
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create(serverUrl + path))
+                .method(method, makeRequestBody(body));
+        if (body != null) {
+            request.setHeader("Content-Type", "application/json");
+        }
+        return request.build();
+    }
+
+    private BodyPublisher makeRequestBody(Object request) {
+        if (request != null) {
+            return BodyPublishers.ofString(new Gson().toJson(request));
+        } else {
+            return BodyPublishers.noBody();
+        }
+    }
+
+    private HttpResponse<String> sendRequest(HttpRequest request) throws DataAccessException {
+        try {
+            return client.send(request, BodyHandlers.ofString());
+        } catch (Exception ex) {
+            throw new DataAccessException(500, ex.getMessage());
+        }
+    }
+
+    private <T> T handleResponse(HttpResponse<String> response, Class<T> responseClass) throws DataAccessException {
+        var status = response.statusCode();
+        if (!isSuccessful(status)) {
+            var body = response.body();
+            if (body != null) {
+                throw new DataAccessException(status, body);
+            }
+
+            throw new DataAccessException(status, "other failure: " + status);
+        }
+
+        if (responseClass != null) {
+            return new Gson().fromJson(response.body(), responseClass);
+        }
+
+        return null;
+    }
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
+    }
+}
