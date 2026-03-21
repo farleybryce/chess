e5fdc2f23566515fc163def3ce15481b69a9bd5a
Bryce Farley <brycesfarley@gmail.com>
2026-03-20 18:58:53 -0600

    impl(p5) set up initial client messages

--- client/src/main/java/client/ChessClient.java
+++ client/src/main/java/client/ChessClient.java
@@ -1,4 +1,50 @@
 package client;
 
+import ui.EscapeSequences.*;
+
+import java.util.Scanner;
+
+import static ui.EscapeSequences.*;
+
 public class ChessClient {
+
+    private String username = null;
+    private final ServerFacade server;
+
+    public ChessClient(String serverUrl) {
+        server = new ServerFacade(serverUrl);
+    }
+
+    public void run() {
+        System.out.println(SET_BG_COLOR_BLACK + SET_TEXT_COLOR_WHITE + "♔ Welcome to Chess. Please Sign in. ♕" + RESET_BG_COLOR + RESET_BG_COLOR);
+        System.out.print(loginHelp());
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            System.out.print(SET_TEXT_COLOR_GREEN + SET_TEXT_FAINT + SET_TEXT_ITALIC
+                    + "Chess Login >>> "
+                    + RESET_TEXT_COLOR + RESET_TEXT_ITALIC + RESET_TEXT_BOLD_FAINT);
+            String line = scanner.nextLine();
+
+            try {
+                result = eval(line);
+                System.out.print(SET_TEXT_COLOR_BLUE + result + RESET_TEXT_COLOR);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println();
+    }
+
+    private String loginHelp() {
+        return """
+                Choose one of the options below:
+                - help
+                - quit
+                - register [username] [password] [email@example.com]
+                - login [username] [password]
+                """;
+    }
 }
--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -1,10 +1,12 @@
 package client;
 
 import chess.*;
+import dataaccess.DataAccessException;
+
 
 public class ClientMain {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        ChessClient client = new ChessClient("http://localhost:8080");
+        client.run();
     }
 }
